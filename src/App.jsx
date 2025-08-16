2aa943cc95f25f0be9d5ce36334c31ba74d2abeb
azrilpramudia <azrilpramudia01@gmail.com>
2025-08-17 00:43:13 +0700

    feat: add custom font

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/section/Hero";
+
 function App() {
   return (
     <>
-      <h1 className="flex text-2xl justify-center items-center font-bold py-5 bg-black text-white">
-        GTA 5 Clone Web App
-      </h1>
+      <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1 className="flex text-2xl justify-center items-center py-3 bg-black text-white font-family-poppins font-bold">
+          Navbar
+        </h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
