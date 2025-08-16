88302788513541d1a4c4e6ac9c08d4e1f061b1f1
azrilpramudia <azrilpramudia01@gmail.com>
2025-08-17 01:01:31 +0700

    feat: add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <h1 className="flex text-2xl justify-center items-center py-3 bg-black text-white font-family-poppins font-bold">
-          Navbar
-        </h1>
+    <nav className="bg-gray-950 text-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 py-5 flex justify-between items-center">
+        {/* Logo */}
+        <div className="text-2xl font-bold font-family-poppins">
+          GTA 5 Clone
+        </div>
+        {/* Navigation Links */}
+        <ul className="flex space-x-6 font-family-poppins font-medium">
+          <li className="hover:text-yellow-400 cursor-pointer">Overview</li>
+          <li className="hover:text-yellow-400 cursor-pointer">GTA Online</li>
+          <li className="hover:text-yellow-400 cursor-pointer">Story</li>
+          <li className="hover:text-yellow-400 cursor-pointer">GTA+</li>
+          <li className="hover:text-yellow-400 cursor-pointer">Community</li>
+          <li className="hover:text-yellow-400 cursor-pointer">Support</li>
+        </ul>
       </div>
     </nav>
   );
