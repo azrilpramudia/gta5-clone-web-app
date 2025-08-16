a7d5d4244815815425eb088774d93f90343dee9d
azrilpramudia <azrilpramudia01@gmail.com>
2025-08-16 23:46:07 +0700

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex text-2xl justify-center items-center font-bold py-5 bg-black text-white">
+        GTA 5 Clone Web App
+      </h1>
+    </>
+  );
+}
+
+export default App;
